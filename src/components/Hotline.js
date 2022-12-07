4549a2f3c7509ec4d8f535d89d98f90117f5f636
camaya21 <cindymamaya21@gmail.com>
2022-12-07 12:22:28 -0500

    Changed insults to Meme & added 1 Hotline

--- src/components/Hotline.js
+++ src/components/Hotline.js
@@ -1,7 +1,14 @@
-function Hot ( props ) {
+function Hotline ( props ) {
     return (
-        <h1>Humble Me</h1>
+        <div>
+            <h1>HOTLINE</h1>
+            <p>
+                Peptoc HOTLINE: <a href="https://www.peptoc.net/hotline">
+                <img border="0" alt="W3Schools" src="https://static.wixstatic.com/media/01c92c_874fa31632064ec7848bb4fcc9c0b3e1~mv2.jpeg/v1/fill/w_594,h_339,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/1E931A16-0575-445A-BF70-0F1972866C14.jpeg" width="200" height="100"/>
+                </a>
+            </p>
+        </div>
     )
 } 
 
-export default Hot
+export default Hotline
---
+++ src/components/Meme.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+function Meme ( props ) {
+    // const [meme, setMeme] = useState (null)
+
+
+    
+
+
+    return (
+        <div>
+            <h1>Pick Me Up</h1>
+            
+        </div>
+    )
+} 
+
+export default Meme
